5fb797dd64d1ee75eda03c0d53b9ec0f373381ee
a.varenyshev <alex.varenyshev@gmail.com>
2023-10-25 23:33:44 +0300

    gg!

--- SearchTests.java
+++ SearchTests.java
@@ -1,4 +1,16 @@
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
 public class SearchTests {
+    @BeforeAll
+    public void init(){
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 240000;
+
     @Test
 void successfulSearchTest() {
     open("https://www.google.com/");
